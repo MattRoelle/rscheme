9b5cf3d03bd47848d9e9a1e92cf27010d953fe96
matt <matt@beast.(none)>
2013-04-03 22:30:53 -0700

    added quotes, fixed n00b stylingz, some other minor tweas

--- main.rb
+++ main.rb
@@ -1,61 +1,93 @@
 require 'babel_bridge'
 
-$builtInProcs = {
-  "+" => Proc.new { |args|
+$built_in_procs = {
+  
+  "+" => proc { |args|
     result = 0
     args.each do  |arg|
       result += arg.to_s.to_f
     end
     result
   },
-  "-" => Proc.new { |args|
+  
+  "-" => proc { |args|
     result = args.first.to_s.to_f
     args[1..args.length].each do |arg|
       result -= arg.to_s.to_f
     end
     result
   },
-  "*" => Proc.new { |args|
+  
+  "*" => proc { |args|
     result = args.first.to_s.to_f
     args[1..args.length].each do |arg|
       result *= arg.to_s.to_f
     end
     result
   },
-  "/" => Proc.new { |args|
+  
+  "/" => proc { |args|
     result = args.first.to_s.to_f
     args[1..args.length].each do |arg|
       result /= arg.to_s.to_f
     end
     result
+  },
+  
+  "print" => proc { |args|
+    print args.first.to_s
   }
+
+}
+
+$built_in_macros = {
+
 }
 
 class Parser < BabelBridge::Parser
 
-  rule :expression, "(", many(:expression, " "), ")" do
+  rule :expression, '\'(', many(:expression, ' '), ')' do
     def evaluate
-      procName = expression[0].to_s
-      procArgs = expression[1..expression.length]
-      newArgs = []
-      procArgs.each do |arg|
-        newArgs << arg.evaluate
+      ret = []
+      expression.each do |e|
+        ret << e.to_s        
       end
-      $builtInProcs[procName].call(newArgs)
-      end
-  end
-  rule :expression, "(", many(:identifier, " "), ")" do
-    def evaluate
-      procName = identifier[0].to_s
-      procArgs = identifier[1..expression.length]
-      return $builtInProcs[procName].call(procArgs)
+      ret
     end
   end
+
+  rule :expression, '\'', :expression do
+    def evaluate
+      expression.to_s
+    end
+  end
+
+  rule :expression, "(", many(:expression, " "), ")" do
+    def evaluate
+      proc_name = expression[0].to_s
+      proc_args = expression[1..expression.length]
+      newArgs = []
+      proc_args.each do |arg|
+        newArgs << arg.evaluate
+      end
+      $built_in_procs[proc_name].call(newArgs)
+      end
+  end
+
+  rule :expression, "(", many(:identifier, " "), ")" do
+    def evaluate
+      proc_name = identifier[0].to_s
+      proc_args = identifier[1..expression.length]
+      return $built_in_procs[proc_name].call(proc_args)
+    end
+  end
+
   rule :expression, :identifier do
     def evaluate
       identifier.evaluate
     end
   end
+
   rule :identifier, /[a-zA-Z_0-9\+\-\*\/]*/ do
     def evaluate
       to_s
